4eab463b26f8b5a271efdd7c0faa51cc73b4d4ac
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-07-18 23:50:32 -0300

    feat: todo list created

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react";
+import { FaRegTrashAlt } from "react-icons/fa";
+
+function App() {
+  const [tasks, setTasks] = useState(() => {
+    const localTasks = localStorage.getItem("tasks")
+    if (!localTasks) {
+      localStorage.setItem("tasks", "[]")
+      return [];
+    } else {
+      return JSON.parse(localTasks)
+    }
+  })
+  const [newTask, setNewTask] = useState("");
+
+  const updateLocalStorageTasks = (newValue) => {
+    localStorage.setItem("tasks", JSON.stringify(newValue));
+  }
+
+  const handleInput = (e) => {
+    setNewTask(e.target.value);
+  }
+
+  const saveTask = (e) => {
+    e.preventDefault();
+    setTasks(prev => {
+      const allTasks = [...prev, { isChecked: false, task: newTask }];
+      updateLocalStorageTasks(allTasks);
+      return allTasks;
+    });
+    setNewTask("");
+  }
+
+  const clearTasks = () => {
+    setTasks([]);
+    updateLocalStorageTasks([]);
+  }
+
+  const changeTaskCheck = (index) => {
+    setTasks(prev => {
+      const copy = [...prev];
+      copy[index] = { isChecked: !copy[index].isChecked, task: prev[index].task };
+      updateLocalStorageTasks(copy);
+      return copy;
+    })
+  }
+
+  const deleteTask = (index) => {
+    setTasks(prev => {
+      const copy = [...prev];
+      copy.splice(index, 1);
+      updateLocalStorageTasks(copy);
+      return copy;
+    })
+  }
+
+
+  return (
+    <form className="main" onSubmit={saveTask}>
+      <input type="text" placeholder="Nova task" value={newTask} onChange={handleInput} className="new-todo-input" />
+
+      {tasks.map((task, index) => (
+        <div className="container-task" key={index} onClick={() => changeTaskCheck(index)}>
+          <div className="checkbox-container">
+            <input type="checkbox" checked={task.isChecked} className="checkbox"/>
+            <label className={task.isChecked ? "task-checked" : "task"}>{task.task}</label>
+          </div>
+          <div className="icon-container">
+            <FaRegTrashAlt className="icon" onClick={() => deleteTask(index)} />
+          </div>
+        </div>
+      ))}
+
+      <button type="button" className="clear-button" onClick={clearTasks}>Limpar tasks</button>
+    </form>
+  );
+}
+
+export default App;
